fac2c20745d1929955ececa991b97a0ce0280cb2
galrothschild <131006606+galrothschild@users.noreply.github.com>
2024-01-28 18:30:42 +0200

    finishing

--- dist/script.js
+++ dist/script.js
@@ -1,23 +1,34 @@
 "use strict";
-let counter = 1;
+let pokemonID = 1;
 document.querySelector(".btn-next")?.addEventListener("click", () => {
-    counter++;
-    console.log(counter);
+    pokemonID++;
+    fetchPokemon(pokemonID);
 });
 document.querySelector(".btn-previous")?.addEventListener("click", () => {
-    counter--;
-    console.log(counter);
+    pokemonID--;
+    fetchPokemon(pokemonID);
 });
-async function getNames() {
-    let results = (await (await fetch("https://pokeapi.co/api/v2/pokemon?limit=1017&offset=0")).json()).results;
-    let longest = results.reduce((acc, curr) => {
-        if (acc.name.length > curr.name.length) {
-            return acc;
-        }
-        else {
-            return curr;
-        }
-    });
-    console.log(longest);
+async function fetchPokemon(id) {
+    try {
+        const pokemon = (await (await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)).json());
+        const pokemonName = pokemon?.name;
+        const pokemonSprite = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/${id}.gif`;
+        setPokemon(pokemonName, id, pokemonSprite);
+    }
+    catch (error) {
+        if (error instanceof Error)
+            throw new Error(error.message);
+        else
+            throw new Error("Unknown Error occured");
+    }
 }
-getNames();
+const setPokemon = (pokemonName, pokemonID, pokemonImage) => {
+    const pokemonNameElement = document.querySelector(".pokemon-name");
+    const pokemonIDElement = document.querySelector(".pokemon-id");
+    const pokemonImgElement = document.querySelector(".pokemon");
+    pokemonNameElement.innerText = pokemonName;
+    pokemonIDElement.innerText = `${pokemonID}`;
+    pokemonImgElement.src = pokemonImage;
+    pokemonImgElement.alt = pokemonName;
+};
+fetchPokemon(1);
--- src/script.ts
+++ src/script.ts
@@ -1,24 +1,36 @@
-let counter: number = 1;
+let pokemonID: number = 1;
 
 document.querySelector(".btn-next")?.addEventListener("click", () => {
-    counter++;
-    console.log(counter);
+    pokemonID++;
+    fetchPokemon(pokemonID);
 });
 document.querySelector(".btn-previous")?.addEventListener("click", () => {
-    counter--;
-    console.log(counter);
+    pokemonID--;
+    fetchPokemon(pokemonID);
 });
 
-async function getNames() {
-    let results: any[] = (await (await fetch("https://pokeapi.co/api/v2/pokemon?limit=1017&offset=0")).json()).results;
-    let longest = results.reduce((acc, curr) => {
-        if (acc.name.length > curr.name.length) {
-            return acc;
-        } else {
-            return curr;
-        }
-    });
-    console.log(longest);
-
+async function fetchPokemon(id: number) {
+    try {
+        const pokemon = (await (await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)).json());
+        const pokemonName: string = pokemon?.name;
+        const pokemonSprite: string = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/${id}.gif`;
+        setPokemon(pokemonName, id, pokemonSprite);
+    } catch (error: unknown) {
+        if (error instanceof Error) throw new Error(error.message);
+        else throw new Error("Unknown Error occured");
+    }
 }
-getNames();
+
+const setPokemon = (pokemonName: string, pokemonID: number, pokemonImage: string) => {
+    const pokemonNameElement = document.querySelector(".pokemon-name") as HTMLSpanElement;
+    const pokemonIDElement = document.querySelector(".pokemon-id") as HTMLSpanElement;
+    const pokemonImgElement = document.querySelector(".pokemon") as HTMLImageElement;
+
+    pokemonNameElement.innerText = pokemonName;
+    pokemonIDElement.innerText = `${pokemonID}`;
+
+    pokemonImgElement.src = pokemonImage;
+    pokemonImgElement.alt = pokemonName;
+
+};
+fetchPokemon(1);
